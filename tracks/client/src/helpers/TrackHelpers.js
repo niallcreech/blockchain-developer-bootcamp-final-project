e93b361214ee41495768047ba76ce768b533d087
niallcreech <niall@sevenmachines.org>
2021-09-27 11:11:43 +0100

    Add more placeholder functions

---
+++ tracks/client/src/helpers/TrackHelpers.js
@@ -0,0 +1,17 @@
+import Web3 from "web3"
+
+export function getTrackNominations(trackId){
+	return null;
+}
+
+export function getAllTrackNominations(){
+	return null;
+}
+
+export function getEntryForTrack(trackId){
+	return null;
+}
+
+export function getAllEntriesForTrack(){
+	return null;
+}
