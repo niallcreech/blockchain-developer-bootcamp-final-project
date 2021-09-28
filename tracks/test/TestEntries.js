22e27e95ef4695d01fe1379c599cd1a042f35247
Niall Creech <niall@sevenmachines.org>
2021-09-28 14:36:08 +0100

    Update tests

--- tracks/test/TestEntries.js
+++ tracks/test/TestEntries.js
@@ -18,8 +18,7 @@ describe("When working with Tracks entries", () => {
       const trackIdVal = await contract.addTrack(trackName, "mydescription", { from: accounts[0]
       });
       const trackId = 1; //parseInt(trackIdVal);
-      const entryName = "myentry"
-      const tx = await contract.addEntry(trackId, entryName, "mydescription", { from: accounts[0] });
+      const tx = await contract.addEntry(trackId, "myentry", "mydescription", { from: accounts[0] });
       const _entries = await contract.getEntriesForTrack(trackId);
       assert.equal(1, _entries.length, "");
     });
@@ -28,8 +27,7 @@ describe("When working with Tracks entries", () => {
       const trackName = "mytrack"
       const tmp = await contract.addTrack(trackName, "mydescription", { from: accounts[0] });
       const trackId = 1; 
-      const entryName = "myentry"
-      const tx = await contract.addEntry(trackId, entryName, "mydescription", { from: accounts[0] });
+      const tx = await contract.addEntry(trackId, "myentry", "mydescription", { from: accounts[0] });
       truffleAssert.eventEmitted(tx, 'EntryCreated', (ev) =>{
         expect(ev.entryId.toNumber()).to.not.equal(0);
         return true;
