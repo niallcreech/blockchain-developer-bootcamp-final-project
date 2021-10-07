629de36f7194d304b68537f62527abd05b200eab
Niall Creech <niall@sevenmachines.org>
2021-10-07 22:24:06 +0100

    Update

--- tracks/client/src/components/HeaderView.js
+++ tracks/client/src/components/HeaderView.js
@@ -37,12 +37,6 @@ class HeaderView extends Component {
 
   constructor(props){
     super(props);
-    this.state = {
-      trackId: null,
-      trackName: null,
-      trackDesc: null,
-      trackDetails: null,
-    }
     this.handleReturnClick = this.handleReturnClick.bind(this);
     this.updateSelectedTrack = this.updateSelectedTrack.bind(this);
   }
@@ -62,7 +56,6 @@ class HeaderView extends Component {
   
   handleReturnClick(){
     this.props.history.push("/");
-    this.updateSelectedTrack();
   }
 
 	render(){
@@ -70,7 +63,7 @@ class HeaderView extends Component {
     if (this.props.match.params.trackId){
       backView = (
         <div>
-          <div trackId={this.state.trackId} className="HeaderViewReturn" onClick={this.handleReturnClick}>Back</div>
+          <div trackId={this.props.trackId} className="HeaderViewReturn" onClick={this.handleReturnClick}>Back</div>
       </div>
       )
     } else {
@@ -79,12 +72,13 @@ class HeaderView extends Component {
       </div>
       )
     }
-    console.debug("HeaderView::render: " + this.state.trackId);
+    console.debug("HeaderView::render: " + this.props.trackId);
+    console.debug(this.props);
     const headerView = (
       <div>
       {backView}
-        <HeaderViewName trackId={this.props.trackId} text={this.state.trackName} />
-        <HeaderViewDesc trackId={this.props.trackId} text={this.state.trackDesc}/>
+        <HeaderViewName trackId={this.props.trackId} text={this.props.trackName} />
+        <HeaderViewDesc trackId={this.props.trackId} text={this.props.trackDesc}/>
       </div>
     );
     
